2c660e1c5b8f58d7b2fd669c57e5bf788e15280b
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-01-13 21:22:24 -0300

    Add miscellaneous System files

---
+++ REPL/system/diagnostics/Diagnostic.cs
@@ -0,0 +1,23 @@
+﻿using REPL.interfaces;
+using REPL.language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REPL.system.diagnostics
+{
+    internal class Diagnostic : IDiagnostic
+    {
+        public Diagnostic(TextSpan span, string message)
+        {
+            Message = message;
+            Span = span;
+        }
+        public string Message { get; }
+        public TextSpan Span { get; }
+
+        public override string ToString() => Message;
+    }
+}
