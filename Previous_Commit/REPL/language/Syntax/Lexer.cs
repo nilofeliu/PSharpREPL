b90553283fe2574c79a402cc6081599f31ae6caf
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-01-20 07:43:25 -0300

    Added Text Files for better input representation
    Also updated the parser and lexer to accept the new
    Text file type syntax.

---
+++ Previous_Commit/REPL/language/Syntax/Lexer.cs
@@ -0,0 +1,215 @@
+﻿using REPL.language.ast;
+
+
+namespace REPL.language.Syntax
+{
+    internal sealed class Lexer
+    {
+        private readonly DiagnosticBag _diagnostics = new();
+        private readonly string _text;
+
+        private int _position;
+        private int _start;
+        private SyntaxKind _kind;
+        private object _value;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+
+        public DiagnosticBag Diagnostics => _diagnostics;
+        
+        private char Current => Peek(0);
+
+        private char Lookahead => Peek(1);
+        private char Lookback => _position > 0 ? Peek(-1) : '\0';
+
+        private char Peek(int offset)
+        {
+            var index = _position + offset;
+
+            if (index >= _text.Length)
+                return '\0';
+            return _text[index];
+        }
+
+        private void Next()
+        {
+            _position++;
+        }
+
+        public SyntaxToken Lex()
+        {
+
+            _start = _position;
+            _kind = SyntaxKind.BadToken;
+            _value = null;
+            
+            
+            switch (Current)
+            {
+                case '\0':
+                    _kind = SyntaxKind.EndOfFileToken;
+                    break;
+                case '+':
+                    _kind = SyntaxKind.PlusToken;
+                    _position++;
+                    break;
+                case '-':
+                    _kind = SyntaxKind.MinusToken;
+                    _position++;
+                    break;
+                case '*':
+                    _kind = SyntaxKind.StarToken;
+                    _position++;
+                    break;
+                case '/':
+                    _kind = SyntaxKind.SlashToken;
+                    _position++;
+                    break;
+                case '(':
+                    _kind = SyntaxKind.OpenParenthesisToken;
+                    _position++;
+                    break;
+                case ')':
+                    _kind = SyntaxKind.CloseParenthesisToken;
+                    _position++;
+                    break;
+                case '&':
+                    if (Lookahead == '&')
+                    {
+                        _kind = SyntaxKind.AmpersandAmpersandToken;
+                        _position += 2;
+                    }
+                    break;
+                case '|':
+                    if (Lookahead == '|')
+                    {
+                        _kind = SyntaxKind.PipePipeToken;
+                        _position += 2;
+                    }
+                    break;
+                case '=':
+                    if (Lookahead == '=')
+                    {
+                        _kind = SyntaxKind.EqualsEqualsToken;
+                        _position += 2;
+                    }
+                    else
+                    {
+                        _kind = SyntaxKind.EqualsToken;
+                        _position++;
+                    }
+                    break;
+                case '!':
+                    if (Lookahead == '=')
+                    {
+                        _kind = SyntaxKind.BangEqualsToken;
+                        _position += 2;
+                    }
+                    else
+                    {
+                        _kind = SyntaxKind.BangToken;
+                        _position++;
+                    }
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    ReadNumberToken();
+                    break;
+                case ' ':
+                case '\t':
+                case '\n':
+                case '\r':
+                    ReadWhiteSpaceToken();
+                    break;
+                case '.':
+                    Next();
+                    break;
+                default:
+                    if (char.IsWhiteSpace(Current))
+                    {
+                        ReadWhiteSpaceToken();
+                    }
+                    else if (IsDispatchExpressionIdentifier() && char.IsLetter(Current))
+                    {
+                        ReadDispatchTokenIdentifier();
+                    }
+                    else if (char.IsLetter(Current))
+                    {
+                        ReadIdentifierTokenOrKeyword();
+                    }
+                    else
+                    {
+                        _diagnostics.ReportBadCharacter(_position, Current);
+                        _position++;
+                    }
+                    break;
+
+            }
+
+            var length = _position - _start;
+            var text = SyntaxFacts.GetText(_kind);
+            if (text == null)
+                text = _text.Substring(_start, length);
+
+            return new SyntaxToken(_kind, _start, text, _value);
+        }
+
+        private bool IsDispatchExpressionIdentifier()
+        {
+            return _text[0] == '.' &&
+                   char.IsLetter(Current);
+        }
+
+        private void ReadDispatchTokenIdentifier()
+        {
+            while (Current != '\n' && Current != '\r' && Current != '\0')
+                Next();  
+            _kind = SyntaxKind.DispatchToken;
+        }
+
+        private void ReadIdentifierTokenOrKeyword()
+        {
+            while (char.IsLetter(Current))
+                Next();
+
+            var length = _position - _start;
+            var text = _text.Substring(_start, length);
+            
+            _kind = SyntaxFacts.GetKeywordKind(text);
+        }
+
+        private void ReadWhiteSpaceToken()
+        {
+            while (char.IsWhiteSpace(Current))
+                Next();
+            _kind = SyntaxKind.WhiteSpaceToken;
+        }
+
+        private void ReadNumberToken()
+        {
+            while (char.IsDigit(Current))
+                Next();
+
+            var length = _position - _start;
+            var text = _text.Substring(_start, length);
+            if (!int.TryParse(text, out var value))
+            {
+                _diagnostics.ReportInvalidNumber(new TextSpan(_start, length), _text, typeof(int));
+            }
+
+            _value = value;
+            _kind = SyntaxKind.NumberToken;
+        }
+    }
+}
