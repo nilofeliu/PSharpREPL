7a5942130d80a46e208edb9484e74d0a99e49109
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-02-27 20:43:50 -0300

    Added SymbolGroup enum to SyntaxSymbol
    for classifing the kinds according to function.

--- PSharp/CodeAnalysis/Symbols/SyntaxSymbol.cs
+++ PSharp/CodeAnalysis/Symbols/SyntaxSymbol.cs
@@ -1,22 +1,65 @@
 ﻿using PSharp.CodeAnalysis.Syntax.Kind;
 
-namespace PSharp.CodeAnalysis.Symbols
+namespace PSharp.CodeAnalysis.Symbols;
+
+public sealed class SyntaxSymbol
 {
-    public sealed class SyntaxSymbol
+    public SyntaxKind Kind { get; }
+    public string Text { get; }
+    public int BinaryPrecedence { get; }
+    public int UnaryPrecedence { get; }
+    public SyntaxGroup Group { get; }
+
+    public SyntaxSymbol(SyntaxKind kind, string text, SyntaxGroup group = SyntaxGroup.None, int binaryPrecedence = 0, int unaryPrecedence = 0)
     {
-        public SyntaxKind Kind { get; }
-        public string Text { get; }
-        public int BinaryPrecedence { get; }
-        public int UnaryPrecedence { get; }
-
-        public SyntaxSymbol(SyntaxKind kind, string text, int binaryPrecedence = 0, int unaryPrecedence = 0)
-        {
-            Kind = kind;
-            Text = text;
-            BinaryPrecedence = binaryPrecedence;
-            UnaryPrecedence = unaryPrecedence;
-        }
+        Kind = kind;
+        Text = text;
+        BinaryPrecedence = binaryPrecedence;
+        UnaryPrecedence = unaryPrecedence;
+        Group = group;
     }
+}
 
-    
+public enum SyntaxGroup
+{
+    None,
+
+    // Operators
+    ArithmeticOperator,
+    ComparisonOperator,
+    LogicalOperator,
+    BitwiseOperator,
+    UnaryOperator,
+    AssignmentOperator,
+    CompoundAssignmentOperator,
+
+    // Tokens
+    Literal,
+    Punctuation,
+    Trivia,
+    SpecialType,
+
+    // Keywords
+    TypeKind,
+    BooleanKeywords,
+    VariableKeywords,
+    AccessModifierKeywords,
+    InheritanceModifierKeywords,
+    ContextualKeywords,
+    ConditionalKeywords,
+    FlowControlStatement,
+    ExceptionKeywords,
+    ReferenceKeywords,
+
+    // Expressions
+    BinaryExpression,
+    UnaryExpression,
+    AssignmentExpression,
+    LiteralExpression,
+
+    // Nodes
+    Statement,
+    Declaration,
+    ComparisonExpression,
+    LogicalExpression,
 }
