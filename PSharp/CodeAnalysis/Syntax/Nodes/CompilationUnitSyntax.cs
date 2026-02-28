6d3bae80b4094ab46279b002e5f8609637ed9151
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-02-28 01:12:26 -0300

    Improved CompilationUnit class

--- PSharp/CodeAnalysis/Syntax/Nodes/CompilationUnitSyntax.cs
+++ PSharp/CodeAnalysis/Syntax/Nodes/CompilationUnitSyntax.cs
@@ -3,33 +3,33 @@ using PSharp.CodeAnalysis.Syntax.Kind;
 
 using PSharp.CodeAnalysis.Syntax.Green;
 
-namespace PSharp.CodeAnalysis.Syntax.Nodes
+namespace PSharp.CodeAnalysis.Syntax.Nodes;
+
+public sealed class CompilationUnitSyntax : SyntaxNode
 {
-    public sealed class CompilationUnitSyntax : SyntaxNode
+    private readonly GreenCompilationUnit _green;
+    private StatementSyntax? _statement;
+
+    internal CompilationUnitSyntax(GreenCompilationUnit green, SyntaxNode? parent, int position)
+        : base(parent, green, position)
     {
-        private readonly GreenCompilationUnit _green;
-        private StatementSyntax? _statement;
-
-        internal CompilationUnitSyntax(GreenCompilationUnit green, SyntaxNode? parent, int position)
-            : base(parent, green, position)
-        {
-            _green = green;
-        }
-
-        public override SyntaxKind Kind => SyntaxKind.CompilationUnit;
-
-        public StatementSyntax Statement
-        {
-            get
-            {
-                if (_statement == null)
-                    _statement = (StatementSyntax)RedNodeFactory.CreateRed(_green.Statement, this, GetChildPosition(0));
-                return _statement;
-            }
-        }
-
-        public SyntaxToken EndOfFileToken
-            => new SyntaxToken(_green.EndOfFileToken, this, GetChildPosition(1));
+        _green = green;
     }
-}
 
+    public override SyntaxKind Kind => SyntaxKind.CompilationUnit;
+
+    public StatementSyntax Statement
+    {
+        get
+        {
+            if (_statement == null)
+                _statement = (StatementSyntax)RedNodeFactory.CreateRed(_green.Statement, this, GetChildPosition(0));
+            return _statement;
+        }
+    }
+    public SyntaxToken EndOfFileToken
+    => new SyntaxToken(_green.EndOfFileToken, this, GetChildPosition(1));
+
+    public override SyntaxToken GetLastToken()
+        => EndOfFileToken;
+}
