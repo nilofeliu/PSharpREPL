c748f4a4c6e1ab136a749ef3021aff304c7655b1
Nilo Martinez Jr <nilofeliu@hotmail.com>
2026-02-28 01:12:09 -0300

    Organized the SyntaxKind and added extensions.

---
+++ PSharp/CodeAnalysis/Syntax/Kind/SyntaxKindExtensions.cs
@@ -0,0 +1,17 @@
+﻿using PSharp.CodeAnalysis.Syntax;
+using PSharp.CodeAnalysis.Syntax.Kind;
+
+public static class SyntaxKindExtensions
+{
+    public static bool IsComparisonOperator(this SyntaxKind kind)
+        => SyntaxFacts.IsComparisonOperator(kind);
+
+    public static bool IsAssignmentOperator(this SyntaxKind kind)
+        => SyntaxFacts.IsAssignmentOperator(kind);
+
+    public static bool IsLogicalOperator(this SyntaxKind kind)
+        => SyntaxFacts.IsLogicalOperators(kind);
+
+    public static bool IsLiteral(this SyntaxKind kind)
+    => SyntaxFacts.IsLiteralToken(kind);
+}
